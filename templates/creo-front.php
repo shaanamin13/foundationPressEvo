82f7c3cf94b96ce7653c459dd2f4814c285f9d8c
shaanamin13 <shaan-amin@uiowa.edu>
2015-12-03 00:01:12 -0600

    Foundation Press for EvoCreo.com

---
+++ templates/creo-front.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Template Name: creo-front
+*/
+get_header(); ?>
+
+<div id="front-hero-1" role="banner">
+	<div class="marketing">
+		<div class="logo">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/evocreo/title2.png"/>
+		</div>
+
+		<div id="watch" class="small-12 columns">
+
+		</div>
+
+
+
+	</div>
+
+</div>
+
+<div id="front-hero-2" role="banner">
+
+<div class="row">
+	<div class="large-6 columns">
+	</div>
+	<div class="large-6 columns">
+		<h4 id="block-grid">Screenshots</h4>
+		<ul class="small-block-grid-2 large-block-grid-3">
+			<li><img class="th" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/evocreo/BANNER1.png" alt=""></li>
+			<li><img class="th" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/evocreo/BANNER5_2.png" alt=""></li>
+			<li><img class="th" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/evocreo/BANNER6_2.png" alt=""></li>
+			<li><img class="th" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/evocreo/BANNER7_2.png" alt=""></li>
+			<li><img class="th" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/evocreo/BANNER8_2.png" alt=""></li>
+		</ul>
+
+	</div>
+	</div>
+
+
+</div>
+<div id="front-hero-3" role="banner">
+
+</div>
+<div id="front-hero-4" role="banner">
+	<div class="row">
+		<h4 id="flex-video">Trailer</h4>
+		<div class="flex-video">
+			<iframe width="460" height="200" src="https://www.youtube.com/embed/BASw7bLZURo" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>
+</div>
